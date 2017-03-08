2eac2b9d4b79ab7aac2b26c801c199f7269eb5c7
Tim Krones <tim@opencraft.com>
2017-03-08 10:03:00 +0100

    Don't require learners to submit form before clicking "Launch", and add logic for opening external URL in new window when learner clicks "Launch".

--- xblock_skytap/public/js/src/skytap.js
+++ xblock_skytap/public/js/src/skytap.js
@@ -3,17 +3,13 @@
 function SkytapXBlock(runtime, element) {
     "use strict";
 
-    var activationForm = $('.skytap-activation-form', element),
-        activateButton = activationForm.find('.skytap-activate'),
-        launchForm = $('.skytap-launch-form', element),
+    var launchForm = $('.skytap-launch-form', element),
         launchButton = launchForm.find('.skytap-launch');
 
-    launchForm.hide();
-
-    activateButton.on('click', function(e) {
+    launchButton.on('click', function(e) {
         e.preventDefault();
-        activationForm.hide();
-        launchForm.show();
+        var sharingPortal = window.open('https://www.skytap.com/', '_blank');
+        sharingPortal.focus();
     });
 
 }

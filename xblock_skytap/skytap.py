c98cbb9fa85fc5423ecce1a4d5fb68ddebd5fd2d
Tim Krones <tim@opencraft.com>
2017-03-08 09:49:57 +0100

    Add configuration fields to match requirements.

--- xblock_skytap/skytap.py
+++ xblock_skytap/skytap.py
@@ -13,6 +13,7 @@ from xblock.fragment import Fragment
 from xblockutils.resources import ResourceLoader
 from xblockutils.studio_editable import StudioEditableXBlockMixin
 
+from .default_data import DEFAULT_DATA
 from .utils import _
 
 
@@ -23,10 +24,28 @@ loader = ResourceLoader(__name__)
 
 # Functions #########################################################
 
+def get_projects():
+    """
+    """
+    return ('Dummy project A', 'Dummy project B', 'Dummy project C')
+
+
 def get_vm_images():
     """
     """
     return ('Dummy image A', 'Dummy image B', 'Dummy image C')
+
+
+def get_vms():
+    """
+    """
+    return ('Dummy VM A', 'Dummy VM B', 'Dummy VM C')
+
+
+def get_subscription_types():
+    """
+    """
+    return ('All', 'Dummy subscription A', 'Dummy subscription B', 'Dummy subscription C')
 
 
 # Classes ###########################################################
@@ -42,14 +61,46 @@ class SkytapXBlock(StudioEditableXBlockMixin, XBlock):
         default=_("Skytap XBlock"),
     )
 
-    vm_image = String(
-        display_name=_("VM image"),
-        help=_("The name of the VM image to use."),
+    project = String(
+        display_name=_("Project"),
+        help=_("Skytap project to pull VM images/templates from."),
+        scope=Scope.settings,
+        values=get_projects,
+    )
+
+    vm_images = String(
+        display_name=_("VM images/Templates"),
+        help=_("List of VM images/templates belonging to this exercise environment."),
         scope=Scope.settings,
         values=get_vm_images,
     )
 
-    editable_fields = ("display_name", "vm_image")
+    vms = String(
+        display_name=_("VMs"),
+        help=_("List of VMs to start for selected template."),
+        scope=Scope.settings,
+        values=get_vms,
+    )
+
+    subscription_types = String(
+        display_name=_("Subscription types"),
+        help=_("List of subscription types that may access this exercise environment."),
+        scope=Scope.settings,
+        values=get_subscription_types,
+    )
+
+    organization_rules = String(
+        display_name=_("Organization rules"),
+        help=_(
+            "Rules that define custom behavior for specific organizations. "
+            "To apply a rule to an organization, add one or more identifiers below the rule name."
+        ),
+        scope=Scope.settings,
+        default=DEFAULT_DATA,
+        multiline_editor=True,
+    )
+
+    editable_fields = ("display_name", "project", "vm_images", "subscription_types", "organization_rules")
 
     def student_view(self, context):
         """

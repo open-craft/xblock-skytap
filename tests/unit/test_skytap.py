2a25965e48ef201253c7937145cc87eb14fa5046
Tim Krones <tim@opencraft.com>
2017-04-05 16:53:39 +0200

    Add unit tests.

--- tests/unit/test_skytap.py
+++ tests/unit/test_skytap.py
@@ -4,8 +4,107 @@ Unit tests for the Skytap XBlock.
 
 import unittest
 
+from mock import Mock
+
+from xblock.field_data import DictFieldData
+
+from xblock_skytap import SkytapXBlock
+
+
+KEYBOARD_LAYOUTS = {  # Sorted by language code
+    "de": "German",
+    "de-ch": "German-Switzerland",
+    "es": "Spanish",
+    "fi": "Finnish",
+    "fr": "French",
+    "fr-be": "French-Belgium",
+    "fr-ch": "French-Switzerland",
+    "is": "Icelandic",
+    "it": "Italian",
+    "jp": "Japanese",
+    "nl-be": "Dutch-Belgium",
+    "no": "Norwegian",
+    "pt": "Polish",
+    "uk": "English (UK)",
+    "us": "English (US)",
+}
+
+XBLOCK_SETTINGS = {
+    "keyboard_layouts": KEYBOARD_LAYOUTS,
+}
+
 
 class TestSkytap(unittest.TestCase):
     """
     Unit tests for the Skytap XBlock.
     """
+
+    def setUp(self):
+        self.service_mock = Mock()
+        self.runtime_mock = Mock()
+        self.runtime_mock.service = Mock(return_value=self.service_mock)
+        self.block = SkytapXBlock(self.runtime_mock, DictFieldData({}), Mock())
+
+    def test_get_keyboard_layouts_no_settings(self):
+        """
+        Test that `get_keyboard_layouts` returns default value
+        if settings service is not available.
+        """
+        self.runtime_mock.service = Mock(return_value=None)
+        self.assertEqual(self.block.get_keyboard_layouts(), {})
+
+    def test_get_keyboard_layouts_no_customizations(self):
+        """
+        Test that `get_keyboard_layouts` returns default value
+        if XBLOCK_SETTINGS do not include customizations for Skytap XBlock.
+        """
+        self.block.get_xblock_settings = Mock(return_value=None)
+        self.assertEqual(self.block.get_keyboard_layouts(), {})
+        self.block.get_xblock_settings.assert_called_once_with(default={})
+
+    def test_get_keyboard_layouts_no_layouts(self):
+        """
+        Test that `get_keyboard_layouts` returns default value
+        if XBLOCK_SETTINGS include customizations for Skytap XBlock,
+        but do not specify keyboard layouts.
+        """
+        xblock_settings = {
+            "other": {},
+            "options": {},
+        }
+        self.block.get_xblock_settings = Mock(return_value=xblock_settings)
+        self.assertEqual(self.block.get_keyboard_layouts(), {})
+        self.block.get_xblock_settings.assert_called_once_with(default={})
+
+    def test_get_keyboard_layouts(self):
+        """
+        Test that `get_keyboard_layouts` returns keyboard layouts as defined in XBLOCK_SETTINGS.
+        """
+        self.block.get_xblock_settings = Mock(return_value=XBLOCK_SETTINGS)
+        self.assertEqual(self.block.get_keyboard_layouts(), KEYBOARD_LAYOUTS)
+        self.block.get_xblock_settings.assert_called_once_with(default={})
+
+    def test_sorted_keyboard_layouts(self):
+        """
+        Test that `sorted_keyboard_layouts` sorts keyboard layouts by language name.
+        """
+        expected_keyboard_layouts = [
+            ("nl-be", "Dutch-Belgium"),
+            ("uk", "English (UK)"),
+            ("us", "English (US)"),
+            ("fi", "Finnish"),
+            ("fr", "French"),
+            ("fr-be", "French-Belgium"),
+            ("fr-ch", "French-Switzerland"),
+            ("de", "German"),
+            ("de-ch", "German-Switzerland"),
+            ("is", "Icelandic"),
+            ("it", "Italian"),
+            ("jp", "Japanese"),
+            ("no", "Norwegian"),
+            ("pt", "Polish"),
+            ("es", "Spanish")
+
+        ]
+        self.block.get_xblock_settings = Mock(return_value=XBLOCK_SETTINGS)
+        self.assertEqual(self.block.sorted_keyboard_layouts, expected_keyboard_layouts)

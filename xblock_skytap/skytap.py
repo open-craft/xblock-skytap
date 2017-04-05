425bc507183831022e381481cf8116d9104615f5
Tim Krones <tim@opencraft.com>
2017-04-05 10:06:03 +0200

    Remove obsolete settings.

--- xblock_skytap/skytap.py
+++ xblock_skytap/skytap.py
@@ -11,39 +11,12 @@ from xblock.fragment import Fragment
 from xblockutils.resources import ResourceLoader
 from xblockutils.studio_editable import StudioEditableXBlockMixin
 
-from .default_data import DEFAULT_DATA
 from .utils import _
 
 
 # Globals ###########################################################
 
 loader = ResourceLoader(__name__)
-
-
-# Functions #########################################################
-
-def get_projects():
-    """
-    """
-    return ('Dummy project A', 'Dummy project B', 'Dummy project C')
-
-
-def get_templates():
-    """
-    """
-    return ('Dummy template A', 'Dummy template B', 'Dummy template C')
-
-
-def get_vms():
-    """
-    """
-    return ('Dummy VM A', 'Dummy VM B', 'Dummy VM C')
-
-
-def get_subscription_types():
-    """
-    """
-    return ('All', 'Dummy subscription A', 'Dummy subscription B', 'Dummy subscription C')
 
 
 # Classes ###########################################################
@@ -59,46 +32,7 @@ class SkytapXBlock(StudioEditableXBlockMixin, XBlock):
         default=_("Skytap XBlock"),
     )
 
-    project = String(
-        display_name=_("Project"),
-        help=_("Skytap project to pull templates from."),
-        scope=Scope.settings,
-        values=get_projects,
-    )
-
-    templates = String(
-        display_name=_("Templates"),
-        help=_("List of templates belonging to this exercise environment."),
-        scope=Scope.settings,
-        values=get_templates,
-    )
-
-    vms = String(
-        display_name=_("VMs"),
-        help=_("List of VMs to start for selected template."),
-        scope=Scope.settings,
-        values=get_vms,
-    )
-
-    subscription_types = String(
-        display_name=_("Subscription types"),
-        help=_("List of subscription types that may access this exercise environment."),
-        scope=Scope.settings,
-        values=get_subscription_types,
-    )
-
-    organization_rules = String(
-        display_name=_("Organization rules"),
-        help=_(
-            "Rules that define custom behavior for specific organizations. "
-            "To apply a rule to an organization, add one or more identifiers below the rule name."
-        ),
-        scope=Scope.settings,
-        default=DEFAULT_DATA,
-        multiline_editor=True,
-    )
-
-    editable_fields = ("display_name", "project", "templates", "subscription_types", "organization_rules")
+    editable_fields = ("display_name",)
 
     def student_view(self, context):
         """

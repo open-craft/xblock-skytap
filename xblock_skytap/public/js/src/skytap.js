9dd29370a39e9ad2479789074c92c437a2fff7e7
Tim Krones <tim@opencraft.com>
2017-04-06 09:22:17 +0200

    Notify learner if browser refuses to open sharing portal link in new tab/window.

--- xblock_skytap/public/js/src/skytap.js
+++ xblock_skytap/public/js/src/skytap.js
@@ -21,7 +21,11 @@ function SkytapXBlock(runtime, element) {
             .success(function(response) {
                 // FIXME: Open sharing portal link from response (OC-2505)
                 var sharingPortal = window.open('https://www.skytap.com/', '_blank');
-                sharingPortal.focus();
+                if (sharingPortal === null) {
+                    alert("The browser's popup blocker prevented the exercise environment from being launched.");
+                } else {
+                    sharingPortal.focus();
+                }
             })
             .error(function(jqXHR, textStatus, errorThrown) {
                 // FIXME: Display appropriate error message (OC-2505)
